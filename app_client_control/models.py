41fc412e92e8fdb4c06a16641f07b6d3d945d2e8
Andres Lemos <Lemos_1325@hotmail.com>
2019-06-08 18:38:31 -0500

    feat: The models are updated

--- app_client_control/models.py
+++ app_client_control/models.py
@@ -4,12 +4,27 @@ from datetime import datetime
 ##################################################################################################
 class Client(models.Model):
     name = models.CharField(max_length=200)
-    phone = models.CharField(max_length=15, blank=True, null=True)
-    registration_date = models.DateTimeField(default=datetime.now)
-    computer_brand = models.CharField(max_length=15, blank=True, null=True)
-    maintenance_date = models.DateTimeField()
+    phone = models.CharField(max_length=15)
+    address = models.CharField(max_length=20)
+    computer_brand = models.CharField(max_length=15)
     description = models.TextField()
+    registration_date = models.DateTimeField(default=datetime.now)
+    maintenance_date = models.DateTimeField()
+    maintenance_required = models.BooleanField(default=False)
 
 	#---------------------------------------------------------------------------------------------
     def __str__ (self):
         return '{}'.format(self.name)
+
+##################################################################################################
+class History(models.Model):
+    client = models.ForeignKey(Client, on_delete=True)
+    phone = models.CharField(max_length=15)
+    address = models.CharField(max_length=20)
+    computer_brand = models.CharField(max_length=15)
+    maintenance_date = models.DateTimeField()
+    description = models.TextField()
+    
+	#---------------------------------------------------------------------------------------------
+    def __str__ (self):
+        return '{}'.format(self.maintenance_date)
