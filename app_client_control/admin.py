94180e80de3975c7c27c5cd718086e6f77af60e4
Andres Lemos <Lemos_1325@hotmail.com>
2019-11-09 14:27:45 -0500

    refactor(Backend): These elements were relocated
    Frontend and Backend separeted

--- app_client_control/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Client
-from .models import History
-
-admin.site.register(Client)
-admin.site.register(History)
--- app_client_control/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppClientControlConfig(AppConfig):
-    name = 'app_client_control'
