59eb7bef9162a3a120264e76a6989549551f46b7
Andres Lemos <Lemos_1325@hotmail.com>
2019-11-10 10:11:45 -0500

    refactor(urls.py-and-wsgi.py): urls added in settings of proyect

---
+++ backend/src/client_control/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import re_path, include
+
+urlpatterns = [
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^api-auth/', include('rest_framework.urls')),
+    re_path(r'^api/', include('app_client_control.api.urls'))
+]
